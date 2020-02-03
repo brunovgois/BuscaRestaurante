d966987e4c6b368035b422945956bcb2e0fa6a49
bruno-gois <brunovgois@gmail.com>
2020-02-03 00:48:31 -0300

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,127 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+} from 'react-native';
 
-// import { Container } from './styles';
-
+import LinearGradient from 'react-native-linear-gradient';
+import isEmail from 'validator/lib/isEmail';
+import {TextInputMask} from 'react-native-masked-text';
+import Icon from 'react-native-vector-icons/Feather';
+import styles from '../styles';
 
 export default class Login extends Component {
-  static navigationOptions = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+
+      errorMailMsg: '',
+      errorPassMsg: '',
+      showEmailErrorMsg: false,
+      showPasswordErrorMsg: false,
+    };
   }
+
+  static navigationOptions = {
+    header: null,
+  };
+
   render() {
     return (
-      <View>
-        <Text>LOGIN</Text>
-      </View>
+      <LinearGradient colors={['#D06600', '#E69A09']} style={styles.gradient}>
+        <Icon
+          name="x"
+          size={40}
+          color="#FFF"
+          onPress={() => this.props.navigation.navigate('Main')}
+          style={styles.backIcon}
+        />
+        <Text style={styles.title}>LOGIN</Text>
+
+        <View style={[styles.inputLabelContainer]}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            value={this.state.email}
+            onChangeText={value => this.setState({email: value})}
+            placeholder="email@email.com"
+            style={styles.input}
+          />
+          {this.state.showEmailErrorMsg && (
+            <Text style={styles.errorMsg}>{this.state.errorMailMsg}</Text>
+          )}
+        </View>
+
+        <View style={styles.inputLabelContainer}>
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            value={this.state.senha}
+            onChangeText={value => this.setState({senha: value})}
+            placeholder="*****"
+            secureTextEntry
+            style={styles.input}
+          />
+          {this.state.showPasswordErrorMsg && (
+            <Text style={styles.errorMsg}>{this.state.errorPassMsg}</Text>
+          )}
+        </View>
+
+        <TouchableHighlight style={styles.LinkContainer}>
+          <Text style={styles.LinkPassword}>ESQUECI MINHA SENHA</Text>
+        </TouchableHighlight>
+
+        <View style={styles.btnContainer}>
+          <TouchableHighlight
+            onPress={() => this.handleConfirmar()}
+            style={styles.btnConfirmar}>
+            <Text style={styles.confirmarText}>CONFIRMAR</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => this.props.navigation.navigate('Cadastro')}
+            style={styles.btnCadastro}>
+            <Text style={styles.linkCadastro}>ME CADASTRAR</Text>
+          </TouchableHighlight>
+        </View>
+      </LinearGradient>
     );
+  }
+
+  handleConfirmar() {
+    const {email, senha, showEmailErrorMsg, showPasswordErrorMsg} = this.state;
+
+    if (email == '') {
+      this.setState({
+        showEmailErrorMsg: true,
+        errorMailMsg: 'Campo de Email é obrigatorio',
+      });
+    } else if (!isEmail(email)) {
+      this.setState({
+        showEmailErrorMsg: true,
+        errorMailMsg: 'Formato de e-mail invalido',
+      });
+    } else {
+      this.setState({showEmailErrorMsg: false});
+    }
+
+    if (senha == '') {
+      this.setState({
+        showPasswordErrorMsg: true,
+        errorPassMsg: 'Campo de Senha é obrigatorio',
+      });
+    } else if (senha.length < 7) {
+      this.setState({
+        showPasswordErrorMsg: true,
+        errorPassMsg: 'Senha deve ter no mínino 7 caracteres',
+      });
+    } else {
+      this.setState({showPasswordErrorMsg: false});
+    }
+
+    if (email && senha && !showEmailErrorMsg && !showPasswordErrorMsg)
+      this.props.navigation.navigate('Main');
   }
 }
---
+++ src/pages/styles.js
@@ -0,0 +1,92 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    borderRadius: 5,
+    backgroundColor: '#FFF',
+    marginHorizontal: 20,
+    fontSize: 16,
+  },
+  inputLabelContainer: {
+    alignSelf: 'stretch',
+    marginTop: 10,
+  },
+  label: {
+    marginLeft: 22,
+    marginBottom: 2,
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 40,
+    position: 'absolute',
+    top: 80,
+  },
+  LinkContainer: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  LinkPassword: {
+    marginRight: 22,
+    color: '#FFF',
+    marginTop: 5,
+    fontSize: 14,
+  },
+  btnContainer: {
+    padding: 5,
+    margin: 5,
+    marginHorizontal: 'auto',
+    position: 'absolute',
+    bottom: 0,
+    flex: 1,
+    width: 200,
+  },
+  btnConfirmar: {
+    backgroundColor: '#f78d23',
+    margin: 5,
+    borderRadius: 5,
+    padding: 10,
+    elevation: 2,
+  },
+  btnCadastro: {
+    marginTop: 5,
+    padding: 5,
+    height: 40,
+  },
+  confirmarText: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  linkCadastro: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  errorMsg: {
+    marginLeft: 25,
+    fontStyle: 'italic',
+    color: '#cc2525',
+    fontWeight: 'bold',
+  },
+  backIcon: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    margin: 20,
+  },
+});
+
+export default styles;
